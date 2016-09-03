62cac5501cb228833327ec0b8bc97a2f97df396c
Miles Elam <miles@geekspeak.org>
2016-09-03 15:43:31 -0700

    Updated image thumbnail handling

--- import-news.sql
+++ import-news.sql
@@ -8,7 +8,7 @@ ALTER TABLE _import ADD COLUMN content text;
 
 \copy _import (newsfeed, source, url, metadata, discussion, labels, content, image_url, remote_id) FROM PSTDIN WITH CSV;
 
-INSERT INTO aggregator.headlines (newsfeed, source, url, metadata, https, discussion, labels, fts, image_url) (SELECT newsfeed, source, url, metadata, url ~ '^https:', discussion, labels, setweight(to_tsvector(coalesce(title,'')), 'A') || setweight(to_tsvector(coalesce(description, '')), 'B') || setweight(to_tsvector(coalesce(content, '')), 'D'), image_url FROM _import) ON CONFLICT DO NOTHING;
+INSERT INTO aggregator.headlines (newsfeed, source, url, metadata, https, discussion, labels, fts, thumbnail) (SELECT newsfeed, source, url, metadata, url ~ '^https:', discussion, labels, setweight(to_tsvector(coalesce(title,'')), 'A') || setweight(to_tsvector(coalesce(description, '')), 'B') || setweight(to_tsvector(coalesce(content, '')), 'D'), thumbnail FROM _import) ON CONFLICT DO NOTHING;
 
 UPDATE aggregator.feeds SET updated = now(), last_id = sub.remote_id FROM (SELECT remote_id FROM _import ORDER BY remote_id DESC LIMIT 1) AS sub WHERE updated + update_interval < now();
 
