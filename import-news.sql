c9be4f5c661b6b7e7ec6dc45fc12ca60a671864b
Miles Elam <miles@geekspeak.org>
2016-09-03 15:07:25 -0700

    feed -> newsfeed

--- import-news.sql
+++ import-news.sql
@@ -5,9 +5,9 @@ CREATE TEMPORARY TABLE _import (LIKE aggregator.headlines INCLUDING DEFAULTS INC
 ALTER TABLE _import DROP COLUMN id;
 ALTER TABLE _import ADD COLUMN remote_id character varying(32);
 
-\copy _import (feed, source, url, title, description, content_type, locale, discussion, labels, content, image_url, remote_id) FROM PSTDIN WITH CSV;
+\copy _import (newsfeed, source, url, title, description, content_type, locale, discussion, labels, content, image_url, remote_id) FROM PSTDIN WITH CSV;
 
-INSERT INTO aggregator.headlines (feed, source, url, title, description, content_type, locale, discussion, labels, content, fts, image_url) (SELECT feed, source, url, title, description, content_type, locale, discussion, labels, content, setweight(to_tsvector(coalesce(title,'')), 'A') || setweight(to_tsvector(coalesce(description, '')), 'B') || setweight(to_tsvector(coalesce(content, '')), 'D'), image_url FROM _import) ON CONFLICT DO NOTHING;
+INSERT INTO aggregator.headlines (newsfeed, source, url, title, description, content_type, locale, discussion, labels, content, fts, image_url) (SELECT newsfeed, source, url, title, description, content_type, locale, discussion, labels, content, setweight(to_tsvector(coalesce(title,'')), 'A') || setweight(to_tsvector(coalesce(description, '')), 'B') || setweight(to_tsvector(coalesce(content, '')), 'D'), image_url FROM _import) ON CONFLICT DO NOTHING;
 
 UPDATE aggregator.feeds SET updated = now(), last_id = sub.remote_id FROM (SELECT remote_id FROM _import ORDER BY remote_id DESC LIMIT 1) AS sub WHERE updated + update_interval < now();
 
