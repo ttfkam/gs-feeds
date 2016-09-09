2e663e679f1225894b0c9d95dae8628e9d2e3fcc
Miles Elam <miles@geekspeak.org>
2016-09-09 12:56:43 -0700

    Fixed typo

--- import-news.sql
+++ import-news.sql
@@ -14,7 +14,7 @@ ALTER TABLE _import ADD COLUMN content text;
 INSERT INTO aggregator.headlines (newsfeed, source, url, metadata, https, discussion, labels, fts, thumbnail) (SELECT newsfeed, source, regexp_replace(url, 'https?:', '', 'i'), metadata, url ~ '^https:', discussion, labels, setweight(to_tsvector(coalesce((metadata->'title')::text,'')), 'A') || setweight(to_tsvector(coalesce((metadata->'description')::text, '')), 'B') || setweight(to_tsvector(coalesce(content, '')), 'D'), thumbnail FROM _import) ON CONFLICT DO NOTHING;
 
 -- Clear the remote id in case we've hit a stall
-UPDATE aggregator.newsfeeds SET remote_id = '' WHERE updated < now() - update_interval;
+UPDATE aggregator.newsfeeds SET last_id = '' WHERE updated < now() - update_interval;
 
 -- Update the remote id with the latest
 UPDATE aggregator.newsfeeds SET updated = now(), last_id = sub.rid FROM (SELECT newsfeed, max(remote_id) as rid FROM _import GROUP BY newsfeed) AS sub;
