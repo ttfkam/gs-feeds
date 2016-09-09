019ff0528ad6c562f175a804236c2790993baed8
Miles Elam <miles@geekspeak.org>
2016-09-09 12:54:58 -0700

    Fix the stall and add comments for clarity to new editors

--- import-news.sql
+++ import-news.sql
@@ -1,16 +1,23 @@
 -- Import articles from feed data
 BEGIN;
 
+-- Make the temp table look very much like the headlines table
 CREATE TEMPORARY TABLE _import (LIKE aggregator.headlines INCLUDING DEFAULTS INCLUDING STORAGE) ON COMMIT DROP;
 ALTER TABLE _import DROP COLUMN id;
 ALTER TABLE _import ADD COLUMN remote_id character varying(32);
 ALTER TABLE _import ADD COLUMN content text;
 
+-- Import the data into the temp table from client's stdin
 \copy _import (newsfeed, source, url, metadata, discussion, labels, content, thumbnail, remote_id) FROM PSTDIN WITH CSV;
 
+-- Copy the new data into the main headlines table
 INSERT INTO aggregator.headlines (newsfeed, source, url, metadata, https, discussion, labels, fts, thumbnail) (SELECT newsfeed, source, regexp_replace(url, 'https?:', '', 'i'), metadata, url ~ '^https:', discussion, labels, setweight(to_tsvector(coalesce((metadata->'title')::text,'')), 'A') || setweight(to_tsvector(coalesce((metadata->'description')::text, '')), 'B') || setweight(to_tsvector(coalesce(content, '')), 'D'), thumbnail FROM _import) ON CONFLICT DO NOTHING;
 
-UPDATE aggregator.newsfeeds SET updated = now(), last_id = sub.remote_id FROM (SELECT remote_id FROM _import ORDER BY remote_id DESC LIMIT 1) AS sub WHERE updated + update_interval < now();
+-- Clear the remote id in case we've hit a stall
+UPDATE aggregator.newsfeeds SET remote_id = '' WHERE updated < now() - update_interval;
+
+-- Update the remote id with the latest
+UPDATE aggregator.newsfeeds SET updated = now(), last_id = sub.rid FROM (SELECT newsfeed, max(remote_id) as rid FROM _import GROUP BY newsfeed) AS sub;
 
 COMMIT;
 
