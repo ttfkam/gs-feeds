2b6a0a7dc8ed435b117e13f2343c4bca283ff17c
Miles Elam <miles@geekspeak.org>
2016-09-24 18:07:45 -0700

    Updated to include content in the stored table

--- import-news.sql
+++ import-news.sql
@@ -5,13 +5,12 @@ BEGIN;
 CREATE TEMPORARY TABLE _import (LIKE aggregator.headlines INCLUDING DEFAULTS INCLUDING STORAGE) ON COMMIT DROP;
 ALTER TABLE _import DROP COLUMN id;
 ALTER TABLE _import ADD COLUMN remote_id character varying(32);
-ALTER TABLE _import ADD COLUMN content text;
 
 -- Import the data into the temp table from client's stdin
 \copy _import (newsfeed, source, url, metadata, discussion, labels, content, thumbnail_url, remote_id) FROM PSTDIN WITH CSV;
 
 -- Copy the new data into the main headlines table
-INSERT INTO aggregator.headlines (newsfeed, source, url, metadata, https, discussion, labels, fts, thumbnail_url) (SELECT newsfeed, source, regexp_replace(url, 'https?:', '', 'i'), metadata, url ~ '^https:', discussion, labels, setweight(to_tsvector(coalesce((metadata->'title')::text,'')), 'A') || setweight(to_tsvector(coalesce((metadata->'description')::text, '')), 'B') || setweight(to_tsvector(coalesce(content, '')), 'D'), thumbnail_url FROM _import) ON CONFLICT DO NOTHING;
+INSERT INTO aggregator.headlines (newsfeed, source, url, metadata, https, discussion, labels, fts, thumbnail_url, content) (SELECT newsfeed, source, regexp_replace(url, 'https?:', '', 'i'), metadata, url ~ '^https:', discussion, labels, setweight(to_tsvector(coalesce((metadata->'title')::text,'')), 'A') || setweight(to_tsvector(coalesce((metadata->'description')::text, '')), 'B') || setweight(to_tsvector(coalesce(content, '')), 'D'), thumbnail_url, content FROM _import) ON CONFLICT DO NOTHING;
 
 -- Clear the remote id in case we've hit a stall
 UPDATE aggregator.newsfeeds SET last_id = '' WHERE updated < now() - update_interval;
